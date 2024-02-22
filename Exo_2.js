83906d55e430a5a85c32ccadbe4e5831f3cbe4f4
Kaiser-G <kaiserg6969@gmail.com>
2024-02-22 17:07:05 +0100

    Refonte de l'exo 2

--- Exo_2.js
+++ Exo_2.js
@@ -1,21 +1,35 @@
 "use strict"; // Oblige la déclaration des variables
-// Les Fonctions/Procédures
+class Employe {
+  // Les attributs privés ont un # il faud faire get et set pour les utiliser
+  Nom;
+  Prenom;
+  Branche;
+  #salaire;
 
-// Zone de déclaration des variables GLOBALES
-// (celles du programme PRINCIPAL)
+  constructor(nom = "par defaud", prenom, branche, salaire) {
+    this.Nom = nom;
+    this.Prenom = prenom;
+    this.Branche = branche;
+    this.#salaire = salaire;
+  }
+
+  //getter
+  get Salaire() {
+    return this.#salaire;
+  }
+  //setter
+  set Salaire(value) {
+    this.#salaire = value;
+  }
+}
+
+const pierre = new Employe("Dupont", "Pierre", "Développement", 2000);
+console.log(pierre);
+
+const paul = new Employe("Durand", "Paul", "Production", 1800);
+console.log(paul);
 
 // Se déclenche lorsque TOUTE LA PAGE est chargée (Style, images etc..)
 window.addEventListener("load", (event) => {
   // Le code de PROGRAMME PRINCIPAL
-
-  class employe {
-    nom;
-    prenom;
-    branche;
-    #salaire;
-
-    constructor(nom = "nc", prenom = "nc", branche = "nc", salaire = "nc") {
-      this.nom = "pierre";
-    }
-  }
 });
