4ffa357e64f89c6af2c9f057272c70d8ac4f0212
Kaiser-G <kaiserg6969@gmail.com>
2024-02-22 14:54:00 +0100

    Début exo 2

---
+++ Exo_2.js
@@ -0,0 +1,21 @@
+"use strict"; // Oblige la déclaration des variables
+// Les Fonctions/Procédures
+
+// Zone de déclaration des variables GLOBALES
+// (celles du programme PRINCIPAL)
+
+// Se déclenche lorsque TOUTE LA PAGE est chargée (Style, images etc..)
+window.addEventListener("load", (event) => {
+  // Le code de PROGRAMME PRINCIPAL
+
+  class employe {
+    nom;
+    prenom;
+    branche;
+    #salaire;
+
+    constructor(nom = "nc", prenom = "nc", branche = "nc", salaire = "nc") {
+      this.nom = "pierre";
+    }
+  }
+});
